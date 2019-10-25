4244fd9999c77282d5ca42a40bf7b1c7f7a4b1e8
Brian <polloking98@gmail.com>
2019-10-25 10:10:28 +0200

    Prueba

--- intercom_buffer.py
+++ intercom_buffer.py
@@ -10,44 +10,6 @@ import argparse                                                                 
 import socket                                                                   # https://docs.python.org/3/library/socket.html
 import queue
 
-<<<<<<< HEAD
-
-if __debug__:
-    import sys
-    
-class Intercom_buffer(Intercom):
-
-    #Definir en init la estructura principal del buffer
-    def init(self, args):
-        Intercom.init(self, args)
-        self.buffer_size=args.buffer_size
-        self.buf=[]
-        for x in range(self.buffer_size):
-            self.list.append([])
-
-        self.num_packet_send=0
-        self.num_packet_receive=0
-        self.bytes=0
-
-    def run(self):
-        sending_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        receiving_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        listening_endpoint = ("0.0.0.0", self.listening_port)
-        receiving_sock.bind(listening_endpoint)
-
-        
-        def receive_and_buffer():
-            message, source_address = receiving_sock.recvfrom(Intercom.max_packet_size)
-            #falta
-            
-            time.sleep(0.5)
-        
-        def record_send_and_play(indata, outdata, frames, time, status):
-            sending_sock.sendto(indata, (self.destination_IP_addr, self.destination_port))
-
-            #falta
-        
-=======
 if __debug__:
     import sys
 
@@ -97,7 +59,6 @@ class Intercom_buffer(Intercom):
         parser = Intercom.add_args(self)
         parser.add_argument("-cb", "--chunks_to_buffer", help="Number of chunks to buffer", type=int, default=32)
         return parser
->>>>>>> 9e190ef425611ee1cef3a5876b5b2ea23f55d8ff
 
 if __name__ == "__main__":
     intercom = Intercom_buffer()
